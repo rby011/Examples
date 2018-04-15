3fe0e4dbb31ef25f9fce07bb396e00be78b162e0
Changsun Song <chsun.song@gmail.com>
2018-04-15 18:35:22 +0900

    add fashion excercise

---
+++ MidExcersize/src/example/fashion/FashionSuggestion.java
@@ -0,0 +1,183 @@
+package example.fashion;
+
+import java.util.Scanner;
+
+public class FashionSuggestion {
+	static final int MAX_N = 5000;
+	static final int MAX_M = 5;
+	static final int MAX_R = 500;
+	static final int MAX_CODE = 10;
+
+	// FOR SIMLIARITY
+	static int weight[] = new int[3];
+
+	// FOR CATALOG DB
+	static int N, K;
+	static int itemN[] = new int[MAX_N];
+	static char code[][][] = new char[MAX_M][MAX_R][MAX_CODE];
+	static int catalog[][] = new int[MAX_N * 2][MAX_M];
+
+	// FOR RANDOM INPUT GENERATION
+	static int seed = 1;
+
+	// FOR TC
+	static int tc;
+
+	// FOR INPUT
+	static Scanner scan = null;
+
+	public static void main(String args[]) {
+
+		scan = new Scanner(System.in);
+
+		int T = scan.nextInt();
+		int score = 0;
+
+		for (tc = 1; tc <= T; tc++) {
+			int r = run();
+			score = score + r;
+
+			System.out.println("# " + tc + " " + r);
+		}
+		System.out.println("# total score : " + score / T);
+
+		scan.close();
+	}
+
+	private static int run() {
+		int score = 100;
+
+		N = scan.nextInt();
+		K = scan.nextInt();
+		seed = scan.nextInt();
+		weight[0] = scan.nextInt();
+		weight[1] = scan.nextInt();
+		weight[2] = scan.nextInt();
+
+		itemN[0] = scan.nextInt();
+		itemN[1] = scan.nextInt();
+		itemN[2] = scan.nextInt();
+		itemN[3] = scan.nextInt();
+		itemN[4] = scan.nextInt();
+
+		makeFashion();
+
+		// INVOKE USER FUNCTION
+		UserCode.init();
+
+		for (int i = 0; i < N; i++) {
+			Fashion fashion = new Fashion();
+			strcpy(fashion.hat, code[0][catalog[i][0]]);
+			strcpy(fashion.top, code[1][catalog[i][1]]);
+			strcpy(fashion.pants, code[2][catalog[i][2]]);
+			strcpy(fashion.shoes, code[3][catalog[i][3]]);
+			strcpy(fashion.accessory, code[4][catalog[i][4]]);
+
+			// INVOKE USER FUNCTION
+			UserCode.addCatalog(fashion);
+		}
+
+		// SCORING
+		for (int i = N; i < (N + K); ++i) {
+			int answer = scan.nextInt();
+
+			Fashion fashion = new Fashion();
+			strcpy(fashion.hat, code[0][catalog[i][0]]);
+			strcpy(fashion.top, code[1][catalog[i][1]]);
+			strcpy(fashion.pants, code[2][catalog[i][2]]);
+			strcpy(fashion.shoes, code[3][catalog[i][3]]);
+			strcpy(fashion.accessory, code[4][catalog[i][4]]);
+
+			// INVOKE USER FUNCTION
+			int result = UserCode.newFashion(fashion);
+			if (result != answer)
+				score = 0;
+		}
+
+		return score;
+	}
+
+	private static void strcpy(char dest[], char src[]) {
+		if (src.length > dest.length)
+			return;
+
+		for (int i = 0; i < src.length; i++)
+			dest[i] = src[i];
+	}
+
+	private static void makeFashion() {
+		for (int i = 0; i < MAX_M; i++) {
+			for (int k = 0; k < itemN[i]; ++k) {
+				int length = 0;
+				for (int m = 0; m < 3; ++m) {
+					code[i][k][length++] = (char) ('A' + pseudoRand() % 26);
+					int num = pseudoRand() % 100;
+					if (num >= 10) {
+						code[i][k][length++] = (char) ('0' + num / 10);
+						num = num % 10;
+						code[i][k][length++] = (char) ('0' + num / 10);
+					} else if (num > 0) {
+						code[i][k][length++] = (char) ('0' + num);
+					}
+				}
+				code[i][k][length] = 0;
+			}
+		}
+
+		for (int i = 0; i < (N + K); ++i) {
+			for (int k = 0; k < MAX_M; ++k) {
+				catalog[i][k] = pseudoRand() % itemN[k];
+			}
+		}
+	}
+
+	private static int pseudoRand() {
+		seed = seed * 1103515245 + 12345;
+		return ((seed / 65536) % 32768) & 0x7fffffff;
+	}
+
+	private static int calcSimliarity(char code1[], char code2[]) {
+		if (new String(code1).equals(new String(code2))) {
+			return 100;
+		}
+		int sum = 0;
+		int index1 = 0, index2 = 0;
+		for (int i = 0; i < 3; i++) {
+			char c1 = code1[index1++];
+			char c2 = code2[index2++];
+			int n1 = 0;
+			while (code1[index1] != 0 && code1[index1] <= '9') {
+				n1 = n1 * 10;
+				n1 = n1 + code1[index1++] - '0';
+			}
+			int n2 = 0;
+			while (code2[index2] != 0 && code2[index2] <= '9') {
+				n2 = n2 * 10;
+				n2 = n2 + code2[index2++] - '0';
+			}
+			sum = sum + ABS((25 - ABS(c1 - c2)) * weight[i] - ABS(n1 - n2)) / 5;
+		}
+
+		if (sum > 99)
+			sum = 99;
+		if (sum < 0)
+			sum = 0;
+
+		return sum;
+	}
+
+	private static int ABS(int num) {
+		return num < 0 ? num * -1 : num;
+	}
+}
+
+class Fashion {
+	static final int MAX_CODE = 10;
+
+	char hat[] = new char[MAX_CODE];
+	char top[] = new char[MAX_CODE];
+	char pants[] = new char[MAX_CODE];
+	char shoes[] = new char[MAX_CODE];
+	char accessory[] = new char[MAX_CODE];
+
+}
---
+++ MidExcersize/src/example/fashion/UserCode.java
@@ -0,0 +1,158 @@
+package example.fashion;
+
+public class UserCode {
+
+	static Hashtable itemtable[] = null;
+
+	static final int HAT_IDX = 0, TOP_IDX = 1, PNT_IDX = 2, SHO_IDX = 3, ACC_IDX = 4;
+	static final int CAPACITY = 5001;
+
+	public static void init() {
+		itemtable = new Hashtable[5];
+		for (int i = 0; i < 5; i++)
+			itemtable[i] = new Hashtable(CAPACITY);
+	}
+
+	public static void addCatalog(Fashion fashion) {
+		itemtable[HAT_IDX].put(fashion.hat, fashion);
+		itemtable[TOP_IDX].put(fashion.top, fashion);
+		itemtable[PNT_IDX].put(fashion.pants, fashion);
+		itemtable[SHO_IDX].put(fashion.shoes, fashion);
+		itemtable[ACC_IDX].put(fashion.accessory, fashion);
+	}
+
+	public static int newFashion(Fashion fashion) {
+		return -1;
+	}
+
+	private static int toindex(char code[]) {
+		
+		return -1;
+	}
+}
+
+class Hashtable {
+
+	int capacity;
+	UFashionList table[] = null;
+
+	public Hashtable(int capacity) {
+		this.table = new UFashionList[capacity];
+		this.capacity = capacity;
+
+		for (int i = 0; i < this.capacity; i++) {
+			table[i] = new UFashionList();
+		}
+	}
+
+	// KEY : fashion code composition
+	public void put(Fashion fashion) {
+		int idx = toindex(hashcode(fashion));
+		table[idx].addFashionToTail(new UFashion(fashion));
+	}
+
+	// KEY : a fashion item code
+	public void put(char item[], Fashion fashion) {
+		int idx = toindex(hashcode(item));
+		table[idx].addFashionToTail(new UFashion(fashion));
+	}
+
+	// THE LIST MAY CONTAINS COLLAPED NODE
+	public UFashionList get(char item[]) {
+		int idx = toindex(hashcode(item));
+		return table[idx];
+	}
+
+	public Fashion get(Fashion fashion) {
+		int idx = toindex(hashcode(fashion));
+		UFashion ufnode = table[idx].head;
+		while (ufnode != null) {
+			if (ufnode.compare(ufnode.fashion)) {
+				return ufnode.fashion;
+			}
+			ufnode = ufnode.next;
+		}
+		return null;
+	}
+
+	public int toindex(int hashcode) {
+		return (hashcode & 0x7fffffff) % this.capacity;
+	}
+
+	public int hashcode(char item[]) {
+		int hash = 1;
+		for (int i = 0; i < item.length && item[i] != 0; i++) {
+			hash = hash * 131 + item[i];
+		}
+		return hash;
+	}
+
+	public int hashcode(Fashion fashion) {
+		int hash = 0;
+
+		hash = hashcode(fashion.hat);
+		hash = hash + hashcode(fashion.top);
+		hash = hash + hashcode(fashion.pants);
+		hash = hash + hashcode(fashion.accessory);
+		hash = hash + hashcode(fashion.shoes);
+
+		return hash;
+	}
+}
+
+class UFashionList {
+	UFashion head;
+
+	public void addFashionToTail(UFashion ufashion) {
+		if (head == null) {
+			this.head = ufashion;
+			return;
+		}
+
+		ufashion.next = head;
+		head = ufashion;
+	}
+}
+
+class UFashion {
+	Fashion fashion;
+
+	UFashion next;
+
+	// FOR BIT OPERATION
+	int uhat = 0;
+	int utop = 0;
+	int upants = 0;
+	int uaccessesory = 0;
+	int ushoes = 0;
+
+	UFashion(Fashion fashion) {
+		this.fashion = fashion;
+	}
+
+	// NEED TO OPTIMZIE WITH BIT OPERATION
+	public boolean compare(Fashion ofashion) {
+		if (compare(fashion.accessory, ofashion.accessory)) {
+			if (compare(fashion.hat, ofashion.hat)) {
+				if (compare(fashion.pants, ofashion.pants)) {
+					if (compare(fashion.shoes, ofashion.shoes)) {
+						if (compare(fashion.top, ofashion.top)) {
+							return true;
+						}
+					}
+				}
+			}
+		}
+		return false;
+	}
+
+	// NEED TO OPTIMZIE WITH BIT OPERATION
+	private boolean compare(char code1[], char code2[]) {
+		for (int i = 0; i < code1.length; i++) {
+			if (code1[i] != code2[i])
+				return false;
+		}
+		return true;
+	}
+
+}
