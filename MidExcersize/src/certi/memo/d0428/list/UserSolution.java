4a4d66b59c65fdfbbdfbf251e82bbad55df799c4
Changsun Song <chsun.song@gmail.com>
2018-04-30 00:32:22 +0900

    fix bugs

--- MidExcersize/src/certi/memo/d0428/list/UserSolution.java
+++ MidExcersize/src/certi/memo/d0428/list/UserSolution.java
@@ -1,6 +1,70 @@
 package certi.memo.d0428.list;
 
+import java.util.Scanner;
+
+/**
+ * <PRE>
+12
+0 1 1 4 4 0000000000000000
+1 5 3 5 3 111111111111111
+2 0 6 8 4 22222222222222222222222222222222
+3 2 2 2 2 3333
+4 6 4 3 2 444444
+5 1 6 4 4 5555555555555555
+6 5 7 3 3 666666666
+7 2 7 2 2 7777
+8 1 2 3 3 888888888
+9 2 3 3 3 999999999
+10 1 3 2 2 AAAA
+11 4 5 1 1 B
+ * </PRE>
+ */
 public class UserSolution {
+
+	public static void main(String args[]) {
+		UserSolution solution = new UserSolution();
+		solution.init(10);
+
+		Scanner scan = new Scanner(System.in);
+
+		int N = scan.nextInt();
+		for (int n = 0; n < N; n++) {
+
+			int mid = scan.nextInt();
+			int y = scan.nextInt();
+			int x = scan.nextInt();
+			int height = scan.nextInt();
+			int width = scan.nextInt();
+			char str[] = new char[100];
+			char tstr[] = scan.next().toCharArray();
+			for (int i = 0; i < tstr.length; i++)
+				str[i] = tstr[i];
+
+			solution.create(mid, y, x, height, width, str);
+		}
+
+		// solution.move(3, 2, 5);
+		// solution.move(10, 6, 6);
+		// solution.move(9, 6, 5);
+		// solution.move(5, 5, 5);
+		// solution.getMemo(3, 2);
+		// solution.select(3);
+		
+		
+		solution.move(3, 3, 3);
+		solution.printmidxtable();
+		
+		solution.getScreenContext(2, 3, null);
+		solution.getScreenContext(1, 1, null);
+		
+		solution.select(9);
+		solution.printmidxtable();
+		solution.getScreenContext(1, 1, null);
+		scan.close();
+
+		// solution.create(0, y, x, height, width, str);
+	}
+
 	MemoIndexList midx_table[][] = null;
 	Memo m_table[] = null;
 
@@ -14,8 +78,8 @@ public class UserSolution {
 	public void create(int mid, int y, int x, int height, int width, char str[]) {
 		Memo memo = new Memo(y, x, height, width, str);
 		m_table[mid] = memo;
-		for (int ry = y; y < y + height; ry++) {
-			for (int rx = x; x < x + width; rx++) {
+		for (int ry = y; ry < y + height; ry++) {
+			for (int rx = x; rx < x + width; rx++) {
 				MemoIndex midx = new MemoIndex(mid);
 				if (midx_table[ry][rx] == null)
 					midx_table[ry][rx] = new MemoIndexList();
@@ -81,7 +145,8 @@ public class UserSolution {
 
 				for (int y = memo.y; y < memo.y + memo.height; y++) {
 					for (int x = sx; x < ex; x++) {
-						midx_table[y][x].removeMemoIndex(mid);
+						if (midx_table[y][x] != null)
+							midx_table[y][x].removeMemoIndex(mid);
 					}
 				}
 
@@ -90,7 +155,8 @@ public class UserSolution {
 
 				for (int y = sy; y < ey; y++) {
 					for (int x = memo.x; x < sx; x++) {
-						midx_table[y][x].removeMemoIndex(mid);
+						if (midx_table[y][x] != null)
+							midx_table[y][x].removeMemoIndex(mid);
 					}
 				}
 			} else if (hdiff > 0 && wdiff > 0) {
@@ -158,15 +224,29 @@ public class UserSolution {
 	public void getScreenContext(int y, int x, char res[][]) {
 		for (int ry = y; ry < y + 5; ry++) {
 			for (int rx = x; rx < x + 5; rx++) {
+				if (ry >= midx_table.length || rx >= midx_table[0].length) {
+					// res[ry][rx] = '\0';
+					System.out.print("_");
+					continue;
+				}
+				if(midx_table[ry][rx] == null) {
+					// res[ry][rx] = '\0';
+					System.out.print("_");
+					continue;
+				}
+				
 				MemoIndex firstidx = midx_table[ry][rx].head;
+				
 				if (firstidx == null) {
-					res[ry][rx] = '\0';
+					// res[ry][rx] = '\0';
+					System.out.print("_");
 				} else {
 					Memo firstmemo = m_table[firstidx.mid];
 					if (firstmemo != null) {
 						int ich = firstmemo.getchar(ry, rx);
 						if (ich != -1) {
-							res[ry][rx] = (char) ich;
+							// res[ry][rx] = (char) ich;
+							System.out.print("" + (char) ich);
 						} else {
 							// error!!
 						}
@@ -175,6 +255,21 @@ public class UserSolution {
 					}
 				}
 			}
+			System.out.println();
+		}
+	}
+
+	public void printmidxtable() {
+		int N = midx_table.length;
+		for (int y = 0; y < N; y++) {
+			for (int x = 0; x < N; x++) {
+				if (midx_table[y][x] != null) {
+					System.out.print(midx_table[y][x].head.mid+"\t");
+				}else {
+					System.out.print("_\t");
+				}
+			}
+			System.out.println();
 		}
 	}
 }
